b1f176199bc8abb0d8d4ad2535b000a14ac1e031
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-04-06 21:40:55 -0300

    add classe NativeInjectorBootstrapper

---
+++ src/ApiTest/IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using Application.Validate;
+
+namespace IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static IServiceCollection RegisterServices(IServiceCollection service)
+        {
+
+
+            service.AddScoped<IUserValidate, UserValidate>();
+
+            return service;
+        }
+
+
+    }
+}
