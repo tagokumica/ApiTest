12b37d707babf9c4e0efd80c339d8e39a5ef5174
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-04-07 17:22:47 -0300

    add UserController

---
+++ src/ApiTest/Api/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interface;
+using Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/usuario")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserValidate _iUserValidate;
+
+        public UserController(IUserValidate iUserValidate)
+        {
+            _iUserValidate = iUserValidate;
+        }
+
+
+        [HttpPost("validar-senha")]
+        public ActionResult<bool> Post(UserViewModel userViewModel)
+        {
+
+            var validate = _iUserValidate.IsValid(userViewModel.Password);
+
+            return validate;
+
+        }
+
+    }
+}
