9d5eeca40b22f6639fea6411c24f50771128f904
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-04-06 21:41:44 -0300

    add classe de test PasswordTest

---
+++ test/Application.Test/User/PasswordTests.cs
@@ -0,0 +1,107 @@
+using Application.Validate;
+using Application.ViewModel;
+using Xunit;
+
+namespace Application.Test.User
+{
+    public class PasswordTests
+    {
+
+
+        [Fact(DisplayName = "Validar Senha com Nove ou mais caracteres")]
+        [Trait("Categoria", "Senha")]
+        public void Password_Validate_NineOrMoreCharacters()
+        {
+            // Arrange
+            var user = new UserViewModel()
+            {
+                Password = "Asdfghjklzxcvb1+"
+            };
+
+            // Act
+            var validationResult = new UserValidate();
+
+            var result = validationResult.IsValid(user.Password);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "Validar Senha com menos caracteres")]
+        [Trait("Categoria", "Senha")]
+        public void Password_Validate_inValidNineOrMoreCharacters()
+        {
+            // Arrange
+            var user = new UserViewModel()
+            {
+                Password = "A1+"
+            };
+
+            // Act
+            var validationResult = new UserValidate();
+
+            var result = validationResult.IsValid(user.Password);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact(DisplayName = "Validar Senha vazia")]
+        [Trait("Categoria", "Senha")]
+        public void Password_Validate_EmptyCharacters()
+        {
+            // Arrange
+            var user = new UserViewModel()
+            {
+                Password = ""
+            };
+
+            // Act
+            var validationResult = new UserValidate();
+
+            var result = validationResult.IsValid(user.Password);
+
+            // Assert
+            Assert.False(result);
+        }
+        [Fact(DisplayName = "Validar Senha Válida")]
+        [Trait("Categoria", "Senha")]
+        public void Password_Validate_PasswordValid()
+        {
+            // Arrange
+            var user = new UserViewModel()
+            {
+                Password = "Asdfghjkl9*"
+            };
+
+            // Act
+            var validationResult = new UserValidate();
+
+            var result = validationResult.IsValid(user.Password);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "Validar Senha Caracter Repetido")]
+        [Trait("Categoria", "Senha")]
+        public void Password_Validate_PasswordCharacterRepeat()
+        {
+            // Arrange
+            var user = new UserViewModel()
+            {
+                Password = "Aasdfghjkl9*"
+            };
+
+            // Act
+            var validationResult = new UserValidate();
+
+            var result = validationResult.IsValid(user.Password);
+
+            // Assert
+            Assert.False(result);
+        }
+
+
+    }
+}
