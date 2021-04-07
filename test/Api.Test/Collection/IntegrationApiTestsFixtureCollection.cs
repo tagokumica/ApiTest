29414361acecef57d6f5d8996113cc69fbf7b7ad
tagokumica <49616408+tagokumica@users.noreply.github.com>
2021-04-07 18:12:35 -0300

    add IntegrationApiTestsFixtureCollection

---
+++ test/Api.Test/Collection/IntegrationApiTestsFixtureCollection.cs
@@ -0,0 +1,11 @@
+﻿using Api.Test.Config;
+using Xunit;
+
+namespace Api.Test.Collection
+{
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection:  ICollectionFixture<IntegrationTestsFixture<Startup>>
+    {
+        
+    }
+}
